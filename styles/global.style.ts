34cbdc5346508a2ff9d9f0e3c67067d028e87571
HomieKim <ghals0708@gmail.com>
2022-07-22 17:13:03 +0900

    design: 웹폰트 추가

--- styles/global.style.ts
+++ styles/global.style.ts
@@ -7,8 +7,8 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     width: 100%;
     height: 100%;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-      Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-family: 'Noto Sans KR', sans-serif,-apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+      Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue;
   }
 
   #__next{
