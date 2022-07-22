0b6348b104ea8dcfeceab5497ad72ebcf1af1576
HomieKim <ghals0708@gmail.com>
2022-07-23 01:00:50 +0900

    feat: type정의 상수입력

--- typings/result.ts
+++ typings/result.ts
@@ -1,2 +1,33 @@
 // 결과 유형 4가지에 대한 타입
-export {}
+export type ResultType = {
+  lv1: {
+    descripton: string;
+    tips: Array<ResultTips>;
+    useLink: boolean;
+  };
+  lv2: {
+    descripton: string;
+    tips: Array<ResultTips>;
+    useLink: boolean;
+  };
+  lv3: {
+    descripton: string;
+    tips: Array<ResultTips>;
+    useLink: boolean;
+  };
+  lv4: {
+    descripton: string;
+    tips: Array<ResultTips>;
+    useLink: boolean;
+  };
+};
+
+type ResultTips = {
+  title: string;
+  tipsDescription?: string;
+  linkList?: Array<{
+    linkDescription: string;
+    linkUrl: string;
+    subTitle?:string;
+  }>;
+};
