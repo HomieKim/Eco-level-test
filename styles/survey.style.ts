d376ffaf5c24e5d43a1f5a0159c56c01752533e4
21yuyu99 <91944385+21yuyu99@users.noreply.github.com>
2022-07-23 07:55:54 +0900

    survey

---
+++ styles/survey.style.ts
@@ -0,0 +1,36 @@
+import style from 'styled-components';
+const SurveyEarthWrapper = style.div`
+position: absolute;
+left: 218px;
+top: 169px;
+`
+const SurveyNumber = style.div`
+  position: absolute;
+  width: 13px;
+  height: 30px;
+  left: 250px;
+  top: 165px;
+  font-weight: 900;
+  font-size: 20px;
+  line-height: 30px;
+  color: #188905;
+`
+const Surveytitle = style.div`
+position: absolute;
+width: 225px;
+height: 72px;
+left: 131px;
+top: 232px;
+font-weight: 500;
+font-size: 16px;
+line-height: 24px;
+color: #000000;
+`
+const SelectButtonWrapper = style.div`
+`
+const SelectText = style.p`
+font-weight: 500;
+font-size: 14px;
+line-height: 21px;
+`
+export {SurveyEarthWrapper,SurveyNumber,Surveytitle,SelectButtonWrapper,SelectText};
