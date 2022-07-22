11e1b2fe0c3628b3c7cbaa59653b7d42238c5b53
21yuyu99 <91944385+21yuyu99@users.noreply.github.com>
2022-07-23 07:57:05 +0900

    survey

--- pages/survey.tsx
+++ pages/survey.tsx
@@ -1,9 +1,33 @@
 import { NextPage } from 'next';
-
+import {SurveyEarthWrapper, SurveyNumber,Surveytitle,SelectButtonWrapper,SelectText} from 'styles/survey.style';
+import Image from 'next/image';
+import {QUESTIONS} from 'utils/constatns';
+import {Button} from 'components/button/button.style'
 const SurVeyPage:NextPage = () => {
   return (
-    <div>테스트 이루어지는 페이지</div>
+    <div>
+      <SurveyEarthWrapper>
+        <Image src='/images/earth.png' width={23} height={23}></Image>
+      </SurveyEarthWrapper>
+      <SurveyNumber>
+        {QUESTIONS[0].qNumber}
+      </SurveyNumber> 
+      <Surveytitle>
+        {QUESTIONS[0].title}
+      </Surveytitle>
+      
+      {QUESTIONS[0].selectList?.map(list=>
+      <SelectButtonWrapper>
+        <Button isPick={false}>
+          <SelectText>{list}</SelectText>
+        </Button>
+        </SelectButtonWrapper>
+        
+        )}
+  
+    </div>
   )
 }
+ 
 
 export default SurVeyPage;
